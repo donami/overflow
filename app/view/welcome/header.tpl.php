985fe37f2fe181a0a17f2d75207932892cd09ae8
Markus Hederström <d0namiz@gmail.com>
2016-06-13 16:17:03 +0200

    Updated about texts

--- app/view/welcome/header.tpl.php
+++ app/view/welcome/header.tpl.php
@@ -1,2 +1,2 @@
-<a href="<?php echo $this->url->create('')?>" class='sitetitle'><?=isset($siteTitle) ? $siteTitle : "Fingerprint"?></a>
+<a href="<?php echo $this->url->create('')?>" class='sitetitle'><?=isset($siteTitle) ? $siteTitle : "Invested"?></a>
 <!-- <span class='siteslogan'><?=isset($siteTagline) ? $siteTagline : "Reusable modules for web development"?></span> -->
